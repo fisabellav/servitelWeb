4fd2f34990243de14b3947fecc3c6d34d1513efc
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-04-30 13:55:30 -0400

    se modifica estructura y crea app crud

--- core/views.py
+++ core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/form.html')
+    return render(request, 'core/index.html')
+
+def catalogo(request):
+    return render(request, 'core/catalogo.html')
+
