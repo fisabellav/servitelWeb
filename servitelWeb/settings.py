25ac8f9f2cd71f895bc61b5813ac6c8cd5062c76
Fernanda Isabella Valenzuela Cabrera <fe.valenzuelac@duocuc.cl>
2025-03-12 21:59:24 -0300

    Update settings.py

--- servitelWeb/settings.py
+++ servitelWeb/settings.py
@@ -39,7 +39,7 @@ MESSAGE_TAGS = {
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-vc4dhc_sc^cg^nw0s&*u(u%q(m34s0=1*kbxwn+igr*19=p5%^')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEVELOPMENT", True) != "False"
